97596bb23f9679509494ac0234595b7402bfa856
moacirdeveloper <moacirdeveloper@gmail.com>
2023-06-28 11:39:29 -0300

    Criaçao MODAL, AppMenu

--- src/componentes/AppMenu.jsx
+++ src/componentes/AppMenu.jsx
@@ -1,18 +1,51 @@
-import React from "react";
+import React, {useState} from "react";
 import "../css/AppMenu.css";
 
-function AppMenu(){
+function AppMenu() {
+
+    const [modal] = useState("fadeAbrir")
+
+    function abreModal(){
+
+        let abreModal = document.querySelector(".fade");
+
+        abreModal.classList.toggle(modal);
+
+    }
+
+    function fechaModal(){
+
+        let fechaModal = document.querySelector(".fade");
+
+        fechaModal.classList.remove(modal);
+
+    }
 
     return (
 
         <div className="navLink">
 
             <ul className="ulNavLink">
-                <li>MODAL</li>
+                <li onClick={() => abreModal()}>MODAL</li>
                 <li>ADM</li>
                 <li>CARRINHO</li>
                 <li>CEP</li>
             </ul>
+
+            <div className="fade">
+                <div className="modal">
+                    <div className="headerModal">
+                        <span>MODAL</span>
+                        <input type="button" className="" onClick={() => fechaModal()} value="Fechar" />
+
+                    </div>
+                    <div className="conteudoModal">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                        <p>Lorem ipsum dolor sit amet.</p>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid, repellendus!</p>
+                    </div>
+                </div>
+            </div>
 
         </div>
     )
