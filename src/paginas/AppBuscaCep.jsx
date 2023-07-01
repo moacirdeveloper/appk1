7bf8146e68317ababb91ec31b098e8a512f29ce5
moacirdeveloper <moacirdeveloper@gmail.com>
2023-07-01 12:22:07 -0300

    Mantenção AppMenu link Cep AppBuscaCep

--- src/paginas/AppBuscaCep.jsx
+++ src/paginas/AppBuscaCep.jsx
@@ -1,15 +1,24 @@
 import React from "react";
-import { useState, useEffect } from "react";
+import { FaSearch } from "react-icons/fa"
+import { useState, useEffect, useRef } from "react";
 
 import "../css/AppBuscaCep.css";
 
 function AppBuscaCep(){
 
+    const [ inputDado, setInputDado ] = useState("");
     const [ cep, setCep ] = useState({});
+    const inputDigitado = useRef();
 
     useEffect( () => {
 
-        fetch("https://viacep.com.br/ws/01001000/json/")
+        if(inputDado === ""){
+            inputDigitado.current.focus();
+            setInputDado("");
+            return; 
+        }
+
+        fetch(`https://viacep.com.br/ws/${inputDado}/json/`)
         .then( dados => dados.json())
         .then(data => {
             setCep(data)
@@ -17,20 +26,36 @@ function AppBuscaCep(){
         })
         .catch((err) => console.log(err))
 
-    }, [])
+        
+
+    }, [inputDado])
+
+    function buscaDadoDigitado() {
+
+        //console.log(inputDigitado.current.value)
+        setInputDado(inputDigitado.current.value);
+        inputDigitado.current.value = "";
+        inputDigitado.current.focus();
+        
+    }
 
     return (
 
         <div className="containerBuscaCep">
 
             <h2>Busca CEP</h2>
+            <div className="valorDigitadoInput">
+                <input ref={inputDigitado} id="inputDiv" type="text" className="inputBuscaCep" />
+                <FaSearch onClick={ () => buscaDadoDigitado()} ></FaSearch>
+            </div>
+
             <div className="dadosBuscaCep">
                 
-                {cep.cep}<br />
-                {cep.logradouro}<br />
-                {cep.complemento}<br />
-                {cep.bairro}<br />
-                {cep.localidade}
+                <span>{cep.cep}</span>
+                <span>{cep.logradouro}</span>
+                <span>{cep.complemento}</span>
+                <span>{cep.bairro}</span>
+                <span>{cep.localidade}</span>
 
 
             </div>
