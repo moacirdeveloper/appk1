3c299080e373252e5ade0249d1f3e7d4f850443d
moacirdeveloper <moacirdeveloper@gmail.com>
2023-07-30 10:08:35 -0300

    AppMenuBar.jsx atualizado para execução

--- src/componentes/AppMenuBar.jsx
+++ src/componentes/AppMenuBar.jsx
@@ -1,8 +1,29 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { useRef } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import "../css/AppMenuBar.css";
 
 function AppMenuBar(){
+
+    let inputDado = useRef();
+    let navigate = useNavigate();
+
+    function buscaDados(){
+
+        const inputBusca = document.getElementById("dadoBusca").value;
+        
+        if(inputBusca){
+
+            navigate(`/editar/${inputBusca}`);
+            // navigate(`/exibir/${inputBusca}`);
+
+        }else{
+
+            alert("Aconteceu um erro Inesperado ! ! !");
+
+        }
+
+    }
 
     return (
 
@@ -12,8 +33,8 @@ function AppMenuBar(){
                 <span><Link to="/exibir">EXIBIR</Link></span>
                 <span><Link to="/cadastrar" >CADASTRAR</Link></span>
                 <div className="buscaDados">
-                    <input type="text" />
-                    <input type="button" value="BUSCAR"/>
+                    <input ref={inputDado} id="dadoBusca" type="text" />
+                    <input type="button" value="BUSCAR" onClick={ () => buscaDados()} />
                 </div>
             </div>
 
